2f76445283a3084f8b96570d06dd10a894646659
Saulius Gražulis <grazulis@ibt.lt>
2016-11-08 10:07:57 +0200

    Changing test case scripts to match the new location of the ReExp files.

--- tests/cases/ere_numbers_004.sh
+++ tests/cases/ere_numbers_004.sh
@@ -3,4 +3,4 @@
 # Test case: test if a provided ERE correctly recognises integer and
 # real (floating-point) numbers.
 
-grep -E "^$(grep -vE '^#|^ *$' numbers.ere)\$" tests/inputs/not-numbers.lst
+grep -E "^$(grep -vE '^#|^ *$' grammars/numbers.ere)\$" tests/inputs/not-numbers.lst
