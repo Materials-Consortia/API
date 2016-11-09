3ee328e4ae3c07b70175d67b88d58f5ef2248cc7
Saulius Gražulis <grazulis@ibt.lt>
2016-11-09 18:34:06 +0200

    Adding a test that can be handled by the full Filter grammar but is rejected by the "flat expression" grammar.

---
+++ tests/cases/ebnf_flat_004.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Test case: test if a provided Filter langauge EBNF grammar correctly
+# recognises filter expressions
+
+./tools/grammatiker/BNF/scripts/grammatica-tree \
+    <(./tools/grammatiker/EBNF/scripts/ebnf2grammatica grammars/flat-filters.ebnf) \
+    <(echo -n filter='b="kva" AND (1<s OR NOT( x!=y AND z>0 ) )')
