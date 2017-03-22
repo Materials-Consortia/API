bb05c82127c9a986a3e13c8ae755dfa37322de0e
Andrius Merkys <andrius.merkys@gmail.com>
2017-03-22 13:48:43 +0200

    Adding response schema for single entry queries, which is essentially the same as entry listing schema, except that the value of the "data" key must have only one entry. Adding a pair of tests.

---
+++ tests/cases/single-entry_001.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+jsonschema -i tests/inputs/cod_query_001.json schemata/single-entry.json
---
+++ tests/cases/single-entry_002.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+jsonschema -i tests/inputs/cod_query_002.json schemata/single-entry.json
