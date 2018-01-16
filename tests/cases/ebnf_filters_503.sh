c5a8df9b769a69258179fe335745d1dfeb87ab72
Saulius Gražulis <grazulis@ibt.lt>
2018-01-16 12:15:32 +0200

    Adding 6 more tests for the new syntax, with partly, fully bracketed and over-bracketed expressions.

---
+++ tests/cases/ebnf_filters_503.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Test case: test if a provided Filter langauge EBNF grammar correctly
+# recognises filter expressions
+
+./tools/grammatiker/BNF/scripts/grammatica-tree \
+    <(./tools/grammatiker/EBNF/scripts/ebnf2grammatica grammars/filters.ebnf) \
+    <(echo -n filter='filter=(NOT a>b AND x>0)')
